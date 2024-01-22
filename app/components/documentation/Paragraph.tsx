ec17873ab8488770189897e4652af78d26334784
suraj dhankad <suraj.dhanked@gmail.com>
2024-01-22 14:51:00 +0530

    added some frontend

--- app/components/documentation/Paragraph.tsx
+++ app/components/documentation/Paragraph.tsx
@@ -1,3 +1,23 @@
-export const Paragraph = () => {
-    <div></div>
-}
+import { cx } from "@/app/lib/cx";
+
+export const Paragraph = ({
+  smallMarginTop = false,
+  children,
+  className = "",
+}: {
+  smallMarginTop?: boolean;
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <p
+      className={cx(
+        smallMarginTop ? "mt-[0.8em]" : "mt-[1.5em]",
+        "text-lg text-gray-700",
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
+};
