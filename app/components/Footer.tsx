71c7b417607ffcb2097ecc11c8d0e8ad702a14ba
suraj dhankad <suraj.dhanked@gmail.com>
2024-01-28 19:02:27 +0530

    added profile

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = () => {
                 <p className="text-sm">&copy; 2024 All Rights Reserved</p>
                 <div className="mt-2 flex items-center justify-center">
                     <img className="rounded-full h-20 w-20 mr-2" src="https://avatars.githubusercontent.com/u/94227272?v=4" alt="Founder" />
-                    <p className="text-s">Founder : <a href="" className="cursor-pointer hover:underline">
+                    <p className="text-s">Founder : <a href="https://www.linkedin.com/in/suraj-dhankad-086458227/" target="_blank" className="cursor-pointer hover:underline">
                         <span>Suraj Dhankad</span>
                     </a></p>
                 </div>
